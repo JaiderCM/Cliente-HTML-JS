7c4376454bfc28dce3e36f729f8fba29946896bb
Javier Alejandro Sanchez Salamanca <83285272+JSanchezUDSoft@users.noreply.github.com>
2023-06-04 15:26:27 -0500

    Funcional

--- Cliente - Js/Cliente - Js/script.js
+++ Cliente - Js/Cliente - Js/script.js
@@ -1,68 +1,52 @@
-class lista extends HTMLElement{
+class holaMundo extends HTMLElement{
   constructor(){
-    super();
-    this.nombreTer;
-    this.interMenor;
-    this.interMayor;
-    this.escala;
+      super();
+      this.a;
+      this.b;
+      this.c;
+      this.d;
   }
 
-  static get observerAttributes(){
-    return['nombreTer', 'interMenor', 'interMayor', 'escala'];
+  static get observedAttributes(){
+      return ['a', "b", "c", "d"];
   }
 
-  attributeChangeCallback(nameAtr, oldValue, newValue){
-    switch(nameAtr){
-      case "nombreTer":
-        this.nombreTer = newValue;
-      break;
-
-      case "interMenor":
-        this.interMenor = newValue;
-      break;
-      
-      case "interMayor":
-        this.interMayor = newValue;
-      break;
-
-      case "escala":
-        this.escala = newValue;
-      break;
-    }
+  attributeChangedCallback(nameAttr, oldValue, newValue){
+      switch(nameAttr){
+          case "a":
+              this.a = newValue;
+          break;
+          case "b":
+              this.b = newValue;
+          break;
+          case "c":
+            this.c = newValue;
+          break;
+          case "d":
+            this.d = newValue;
+          break;
+      }
   }
 
   connectedCallback(){
-    this.innerHTML = `<div class = "vista" id="Termo">
+      this.innerHTML = `<div>
+      <div class="botones">
+      <h3> Nombre del Termómetro: </h3>
+      <input type="text" id="mostrarNom" value="${this.a}">
+  </div>
 
-    <div class="botones">
-        <h3> Nombre del Termómetro: </h3>
-        <input type="text" id="mostrarNom" value="${this.nombreTer}">
-    </div>   
+  <div class="botones">
+      <h3> La temperatura del Termómetro es: </h3>
+      <input type="text" id="grados">
+  </div>
 
-    <div class="botones">
-        <h3> La temperatura del Termómetro es: </h3>
-        <input type="text" id="grados">
-    </div>
+  <input type="range" id="temperatura" min="${this.b}" max="${this.c}" step="${this.d}" value="0">
+  </div></div>`;
 
-    <input type="range" id="temperatura" min="${this.interMenor}" max="${this.interMayor}" step="${this.escala}" value="0">
-        <!--<div id="termometro">
-            <div id="indicador"></div>
-        </div>
-
-    </div>-->
-
-    <div class="vista" id="infoTermo">
-        <h3> Estado de conexión <input id="Estado"> </h3>
-        <h3>Dirección IP: <input type="number"></h3>
-        <h3>Puerto: <input type="number"></h3>
-        <div class="botones"><button class="btnConexion"> Conectar </button> <button class="btnConexion"> Desconectar </button></div>
-    </div>
-</div>`;
   }
-
 }
 
-window.customElements.define("nuevo-termo", lista);
+window.customElements.define("nuevo-termo", holaMundo);
 
 
 const input = document.getElementById("temperatura");
@@ -79,21 +63,24 @@ function guardarInfo(){
   termos[2] = document.getElementById("interMayor").value;
   termos[3] = document.getElementById("escala").value;
 
-  document.getElementById("mostrarNom").value = termos[0];
-
+  /*document.getElementById("mostrarNom").value = termos[0];
     input.setAttribute("min", interMenor);
     input.setAttribute("max", interMayor);
-    input.setAttribute("step", escala);
+    input.setAttribute("step", escala);*/
+
+    
+
+    document.getElementById('Termo').innerHTML += '<nuevo-termo a="'+termos[0]+'" b="'+termos[1]+'" c="'+termos[2]+'" d="'+termos[3]+'"></nuevo-termo>';
 
     clear();
 
 }
 
-input.addEventListener("input", () => {
+/*input.addEventListener("input", () => {
   document.getElementById("grados").value = input.value;
   const value = input.value;
   indicador.style.height = value + "px";
-});
+});*/
 
 function clear(){
   document.getElementById("nombreTer").value = "";
